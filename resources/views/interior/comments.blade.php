6d093a39df684b40675a6fd57d221a68f945a456
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-09-07 01:34:08 +0100

    Edit Controller Files

--- resources/views/interior/comments.blade.php
+++ resources/views/interior/comments.blade.php
@@ -11,168 +11,35 @@
 									Latest Comments
 								</h4>
 									<ul class="list1 no-bullets">
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/01.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Alex Walker
-														<small>2 hours ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, corporis. Voluptatibus odio perspiciatis non quisquam provident, quasi eaque officia.
+										@foreach ([] as $element)
+											<li class="item-editable">
+												<div class="media">
+													<div class="item-edit-controls darklinks">
+														<a href="#">
+															<i class="fa fa-share-square-o"></i>
+														</a>
+														<a href="#">
+															<i class="fa fa-edit"></i>
+														</a>
+														<a href="#">
+															<i class="fa fa-trash-o"></i>
+														</a>
+													</div>
+													<div class="media-left">
+														<img src="images/team_square/01.jpg" alt="...">
+													</div>
+													<div class="media-body">
+														<h5>
+															Alex Walker
+															<small>2 hours ago</small>
+														</h5>
+														<div>
+															Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, corporis. Voluptatibus odio perspiciatis non quisquam provident, quasi eaque officia.
+														</div>
 													</div>
 												</div>
-											</div>
-										</li>
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/02.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Janet C. Donnalds
-														<small>5 hours ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero itaque dolor laboriosam dolores magnam mollitia, voluptatibus inventore accusamus illo.
-													</div>
-												</div>
-											</div>
-										</li>
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/03.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Victoria Grow
-														<small>1 day ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse, magni aliquam quisquam modi delectus veritatis est ut culpa minus repellendus.
-													</div>
-												</div>
-											</div>
-										</li>
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/01.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Alex Walker
-														<small>2 hours ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, corporis. Voluptatibus odio perspiciatis non quisquam provident, quasi eaque officia.
-													</div>
-												</div>
-											</div>
-										</li>
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/02.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Janet C. Donnalds
-														<small>5 hours ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero itaque dolor laboriosam dolores magnam mollitia, voluptatibus inventore accusamus illo.
-													</div>
-												</div>
-											</div>
-										</li>
-										<li class="item-editable">
-											<div class="media">
-												<div class="item-edit-controls darklinks">
-													<a href="#">
-														<i class="fa fa-share-square-o"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-edit"></i>
-													</a>
-													<a href="#">
-														<i class="fa fa-trash-o"></i>
-													</a>
-												</div>
-												<div class="media-left">
-													<img src="images/team_square/03.jpg" alt="...">
-												</div>
-												<div class="media-body">
-													<h5>
-														Victoria Grow
-														<small>1 day ago</small>
-													</h5>
-													<div>
-														Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, esse, magni aliquam quisquam modi delectus veritatis est ut culpa minus repellendus.
-													</div>
-												</div>
-											</div>
-										</li>
+											</li>
+										@endforeach
 									</ul>
 								</div>
 							</div>
