6e0ec06e1f8797acfdd7649e220f7a6106e559f9
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-09-09 02:44:19 +0100

    Update NMBG Table

--- app/NMBGPicture.php
+++ app/NMBGPicture.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class NMBGPicture extends Model
 {
-    //
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'picture',
+        'description',
+        'short_description',
+        'categories',
+    ];
 }
--- database/migrations/2018_09_03_202128_create_n_m_b_g_pictures_table.php
+++ database/migrations/2018_09_03_202128_create_n_m_b_g_pictures_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateNMBGPicturesTable extends Migration
 {
@@ -15,6 +15,13 @@ class CreateNMBGPicturesTable extends Migration
     {
         Schema::create('n_m_b_g_pictures', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('picture');
+            $table->longText('description')->nullable();
+            $table->longText('short_description')->nullable();
+            $table->string('categories');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
