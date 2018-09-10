158d2b266be36642900b3c0b1f43f03df1a6470c
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-09-10 02:17:53 +0100

    Use Black

--- config/voyager.php
+++ config/voyager.php
@@ -204,7 +204,7 @@ return [
     |
      */
 
-    'primary_color' => '#22A7F0',
+    'primary_color' => '#000000',
 
     'show_dev_tips' => true, // Show development tip "How To Use:" in Menu and Settings
 
