464d5e95368d9f6fee67f0aeb288ddaaec91009b
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-09-13 14:05:17 +0100

    Add favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,6 +14,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <link rel="shortcut icon" href="/favicon.ico">
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
