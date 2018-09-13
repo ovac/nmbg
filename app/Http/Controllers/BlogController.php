bb4d7a093bac808d69781787a29f063e9ddee446
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-09-13 13:53:04 +0100

    Desc Blogs on Homepage

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -15,7 +15,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $blogs = Blog::paginate();
+        $blogs = Blog::orderBy('created_at', 'DESC')->paginate();
 
         return view('blogs.index', compact('blogs'));
     }
