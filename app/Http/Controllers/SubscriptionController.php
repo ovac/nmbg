cff91cf63fba5d05956af61fe7570facb0fcebbb
Victor Ariama (ovac4u) www.ovac4u.com <iamovac@gmail.com>
2018-10-02 22:28:51 +0000

    Payment processing

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -80,7 +80,7 @@ class SubscriptionController extends Controller
                 ->channel($request->input('channel'))
 
             //- The URL to send callback after payment.
-                ->callback(url('/subscription-activation/' . auth()->id()))
+                ->callback(url('subscription-activation/' . auth()->id()))
 
             //- Run the transaction after required data.
                 ->run();
